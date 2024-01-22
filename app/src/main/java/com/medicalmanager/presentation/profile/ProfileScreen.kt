e5a4c0779ecd10f7c1712fc115dd86bcb211e797
mutukuian <mutukui940@gmail.com>
2024-01-22 22:06:56 +0300

    implemented Ui for profile screen

--- app/src/main/java/com/medicalmanager/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/medicalmanager/presentation/profile/ProfileScreen.kt
@@ -1,18 +1,339 @@
 package com.medicalmanager.presentation.profile
 
-import android.widget.Toast
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.compose.rememberNavController
-import com.medicalmanager.presentation.authentication.auth_screen.SignOutBtn
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.medicalmanager.R
+import com.medicalmanager.presentation.ui.theme.LightPrimaryColor
+import com.medicalmanager.presentation.ui.theme.Poppins
+import com.medicalmanager.presentation.ui.theme.PrimaryColor
+import com.medicalmanager.presentation.ui.theme.SecondaryColor
+import com.medicalmanager.presentation.ui.theme.Shapes
+
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ProfileScreen(){
 
-    val navController = rememberNavController()
+    //var scrollstate by rememberScrollState()
+    Box (modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.background)){
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            HeaderText()
+            Spacer(modifier = Modifier.height(16.dp))
 
-    SignOutBtn(navController = navController)
+            ProfileCardUI()
+            Spacer(modifier = Modifier.height(16.dp))
+
+            GeneralOptionsUI()
+            Spacer(modifier = Modifier.height(16.dp))
+
+            SupportOptionsUI()
+        }
 
 
+    }
 
+
+}
+
+
+@Composable
+fun HeaderText() {
+    Text(
+        text = "Settings",
+        fontFamily = Poppins,
+        color = SecondaryColor,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 30.dp, bottom = 10.dp),
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
+    )
+}
+
+@Composable
+fun ProfileCardUI() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(10.dp),
+        backgroundColor = Color.White,
+        elevation = 0.dp,
+        shape = Shapes.large
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column() {
+                Text(
+                    text = "Check Your Profile",
+                    fontFamily = Poppins,
+                    color = SecondaryColor,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Text(
+                    text = "UI.Stack.YT@gmail.com",
+                    fontFamily = Poppins,
+                    color = Color.Gray,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+
+                Button(
+                    modifier = Modifier.padding(top = 10.dp),
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = PrimaryColor
+                    ),
+                    contentPadding = PaddingValues(horizontal = 30.dp),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 2.dp
+                    ),
+                    shape = Shapes.medium
+                ) {
+                    Text(
+                        text = "View",
+                        fontFamily = Poppins,
+                        color = SecondaryColor,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile_card_image),
+                contentDescription = "",
+                modifier = Modifier.height(120.dp)
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun GeneralOptionsUI() {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 14.dp)
+            .padding(top = 10.dp)
+    ) {
+        Text(
+            text = "General",
+            fontFamily = Poppins,
+            color = SecondaryColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+        )
+        GeneralSettingItem(
+            icon = R.drawable.ic_rounded_notification,
+            mainText = "Notifications",
+            subText = "Customize notifications",
+            onClick = {}
+        )
+        GeneralSettingItem(
+            icon = R.drawable.ic_more,
+            mainText = "More customization",
+            subText = "Customize it more to fit your usage",
+            onClick = {}
+        )
+//        GeneralSettingItem()
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun GeneralSettingItem(icon: Int, mainText: String, subText: String, onClick: () -> Unit) {
+    Card(
+        onClick = { onClick() },
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .fillMaxWidth(),
+        elevation = 0.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .size(34.dp)
+                        .clip(shape = Shapes.medium)
+                        .background(LightPrimaryColor)
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = "",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(14.dp))
+                Column(
+                    modifier = Modifier.offset(y = (2).dp)
+                ) {
+                    Text(
+                        text = mainText,
+                        fontFamily = Poppins,
+                        color = SecondaryColor,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    Text(
+                        text = subText,
+                        fontFamily = Poppins,
+                        color = Color.Gray,
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.offset(y = (-4).dp)
+                    )
+                }
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_right_arrow),
+                contentDescription = "",
+                modifier = Modifier.size(16.dp)
+            )
+
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun SupportOptionsUI() {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 14.dp)
+            .padding(top = 10.dp)
+    ) {
+        Text(
+            text = "Support",
+            fontFamily = Poppins,
+            color = SecondaryColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+        )
+        SupportItem(
+            icon = R.drawable.ic_whatsapp,
+            mainText = "Contact",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_feedback,
+            mainText = "Feedback",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_privacy_policy,
+            mainText = "Privacy Policy",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_about,
+            mainText = "About",
+            onClick = {}
+        )
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun SupportItem(icon: Int, mainText: String, onClick: () -> Unit) {
+    Card(
+        onClick = { onClick() },
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .fillMaxWidth(),
+        elevation = 0.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(34.dp)
+                        .clip(shape = Shapes.medium)
+                        .background(LightPrimaryColor)
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = "",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(14.dp))
+
+                Text(
+                    text = mainText,
+                    fontFamily = Poppins,
+                    color = SecondaryColor,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_right_arrow),
+                contentDescription = "",
+                modifier = Modifier.size(16.dp)
+            )
+
+        }
+    }
 }
--- app/src/main/java/com/medicalmanager/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/medicalmanager/presentation/ui/theme/Type.kt
@@ -2,11 +2,16 @@ package com.medicalmanager.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.medicalmanager.R
 
 // Set of Material typography styles to start with
+val ReemKufi = FontFamily(Font(R.font.reemkufi))
+val Poppins = FontFamily(Font(R.font.poppins))
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
